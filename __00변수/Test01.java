64752c179c418adc90da922fdbbdc1ea2f1a5617
Jungon Jo <jojungon@gmail.com>
2024-08-06 12:44:47 +0900

    modify file dir

--- __00변수/Test01.java
+++ __00변수/Test01.java
@@ -3,13 +3,16 @@ package __00변수;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 프로그램 시작점을 알리는 코드
+		// sysout + ctrl + space
 		// 정수 변수 선언
 		int a = 10;
 		byte aa = 10;
 		System.out.println(a);
 		System.out.println(aa);
-		// 소스 실행하는 단축키 ; Ctrl + f11
+		// system.out.println()
+		// 괄호 안의 내용을 콘솔에 출력하고 줄 바꿈하시오.
+		// 소스 실행하는 단축키 : ctrl + F11(jre가 실행할 수 있도록 함/compile + 실행)
 		
 		// 문자열 변수 선언
 		String s = "Jungon Jo";
