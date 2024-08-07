c4e74ccce120ee0ddc478d983f255442e454c2c8
Jungon Jo <jojungon@gmail.com>
2024-08-07 17:27:42 +0900

    thinking code

---
+++ __02If/thinking.java
@@ -0,0 +1,55 @@
+package __02If;
+
+public class thinking {
+
+	public static void main(String[] args) {
+		int a = 8;
+		
+		//코드1
+		if(a==1) {
+			System.out.println("1입니다.");
+		}else if(a==2) {
+			System.out.println("1입니다.");
+		}else if(a==3) {
+			System.out.println("1입니다.");
+		}else if(a==4) {
+			System.out.println("1입니다.");
+		}else if(a==5) {
+			System.out.println("1입니다.");
+		}else if(a==6) {
+			System.out.println("1입니다.");
+		}else if(a==7) {
+			System.out.println("1입니다.");
+		}else if(a==8) {
+			System.out.println("1입니다.");
+		}else if(a==9) {
+			System.out.println("1입니다.");
+		}
+		
+		//코드2
+		if(a>0) {
+			if(a==1) {
+				System.out.println("1입니다.");
+			}else if(a==2) {
+				System.out.println("1입니다.");
+			}else if(a==3) {
+				System.out.println("1입니다.");
+			}else if(a==4) {
+				System.out.println("1입니다.");
+			}else if(a==5) {
+				System.out.println("1입니다.");
+			}
+		}else if(a>5){
+			if(a==6) {
+				System.out.println("1입니다.");
+			}else if(a==7) {
+				System.out.println("1입니다.");
+			}else if(a==8) {
+				System.out.println("1입니다.");
+			}else if(a==9) {
+				System.out.println("1입니다.");
+			}
+		}
+	}
+
+}
