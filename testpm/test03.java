6a8c72e0a87a12210175211ea278631bb6c05aa1
Jungon Jo <jojungon@gmail.com>
2024-08-07 16:12:42 +0900

    modify testpm test03

--- testpm/test03.java
+++ testpm/test03.java
@@ -56,7 +56,26 @@ public class test03 {
 		 * 홀수인 로또 번호의 합을 구하시오.. 결과 : 33 + 5 = 38
 		 */
 		int[] lotto3 = {6, 12, 33, 4, 5, 26};
-		System.out.println(lotto3[0] + lotto3[2] + lotto3[4]);
+		int oddSum = 0;
+		if (lotto3[0]%2==1) {
+			oddSum += lotto3[0];
+		}
+		if (lotto3[1]%2==1) {
+			oddSum += lotto3[1];
+		}
+		if (lotto3[2]%2==1) {
+			oddSum += lotto3[2];
+		}
+		if (lotto3[3]%2==1) {
+			oddSum += lotto3[3];
+		}
+		if (lotto3[4]%2==1) {
+			oddSum += lotto3[4];
+		}
+		if (lotto3[5]%2==1) {
+			oddSum += lotto3[5];
+		}
+		System.out.println(oddSum);
 		System.out.println("---------------------------------------------------------------------");
 		System.out.println("문제6");
 		/* int[] a = {6, 12, 33, 4, 5, 26}; (배열의 값은 바뀔 수 있다.)
