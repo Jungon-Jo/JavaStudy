55c8a85aa4599eea3520700c4779798088060f2c
Jungon Jo <jojungon@gmail.com>
2024-08-20 16:29:58 +0900

    change sysout of num4

--- testpm/test_09/test09_04.java
+++ testpm/test_09/test09_04.java
@@ -13,7 +13,7 @@ public class test09_04 {
 		// 자동차 번호의 마지막 번호는 해당 차량이 주차할 주차번호이다.
 		// 만약 주차번호에 다른 차량이 주차되어 있으면 다음 주차번호에 주차를 한다.
 		// (이때, 다음 주차번호에도 차량이 있으면 주차불가이다.)
-		// 주차가 끝난 후 주차번호와 주차항 차량 번호를 출력하시오.
+		// 주차가 끝난 후 주차번호와 주차한 차량 번호를 출력하시오.
 		int[] carnum = {1232,1221,1235,1252,1234,4536,3457,3238,3229,2599};
 		int[] parking = new int[10];
 		for (int i = 0; i < carnum.length; i++) {
@@ -36,6 +36,9 @@ public class test09_04 {
 			}
 		}
 		System.out.println(Arrays.toString(parking));
+		for (int i = 0; i < parking.length; i++) {
+			System.out.println(i + "번에 " + parking[i]);
+		}
 	}
 
 }
