0917be124d653674f9b7c04d5a826b016f3a84f4
Jungon Jo <jojungon@gmail.com>
2024-09-20 17:27:12 +0900

    object test

--- testpm/test_14/Train_info.java
+++ testpm/test_14/Train_info.java
@@ -4,17 +4,41 @@ import java.util.Scanner;
 
 public class Train_info {
 	Scanner in = new Scanner(System.in);
-	String trainNum = null;
+	int[] trainNumArray = {1111, 2222, 3333, 4444};
+	String[] trainTypeArray = {"무궁화", "새마을"};
+	int trainNum = 0;
+	String trainType = null;
 	String trainTime = null;
 	Train_info() {
-		time();
+		
+	}
+	
+	public void number(int enterNum) {
+		for(int i = 0; i < trainNumArray.length; i++) {
+			if(trainNumArray[i] == enterNum) {
+				if(i%2==0) {
+					trainNum = enterNum;
+					trainType = "무궁화";
+					break;
+				} else {
+					trainNum = enterNum;
+					trainType = "새마을";
+					break;
+				}
+			}
+		}		
 	}
 	
 	public void time() {
-		System.out.println("기차 출발 時를 입력하시오.");
+		System.out.println("기차 도착 時를 입력하시오.");
 		String trainHour = in.nextLine();
-		System.out.println("기차 출발 分을 입력하시오.");
+		System.out.println("기차 도착 分을 입력하시오.");
 		String trainMin = in.nextLine();
 		trainTime = trainHour + trainMin;
+	}
+
+	
+	public void prt() {
+		System.out.println(trainNum + " : " + trainType + " : " + trainTime);
 	}
 }
--- testpm/test_14/Train_menu.java
+++ testpm/test_14/Train_menu.java
@@ -1,12 +1,14 @@
 package testpm.test_14;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Train_menu {
-	Train_info[] trainList = new Train_info[5];
+	Train_info[] trainList = new Train_info[4];
 	int tL = trainList.length;
 	Scanner in = new Scanner(System.in);
 	Train_menu() {
+		basicInfo();
 		while(true) {
 			menu();
 			int selNum = in.nextInt();
@@ -32,24 +34,67 @@ public class Train_menu {
 		System.out.println("4. 종료");
 	}
 	
+	public void basicInfo() {
+		int[] trainNumArray = {1111, 2222, 3333, 4444};
+		for(int i = 0; i < trainNumArray.length; i++) {
+			Train_info trainInfo = new Train_info();
+			trainInfo.number(trainNumArray[i]);
+			trainList[i] = trainInfo;
+		}
+	}
+	
 	public void enroll() {
-		Train_info trainInfo = new Train_info();
-		System.out.println("기차 번호를 입력하시오.");
-		trainInfo.trainNum = in.nextLine();
+		int[] trainNumArray = {1111, 2222, 3333, 4444};
+		System.out.println("등록을 원하시는 기차번호를 입력하여 주세요.");
+		int trainNum = in.nextInt();
+		in.nextLine();
 		for(int i = 0; i < tL; i++) {
-			if(trainList[i]==null) {
-				trainInfo.time();
-				trainList[i]=trainInfo;
+			if(IntStream.of(trainNumArray).anyMatch(x -> x == trainNum)) {
+				if(trainList[i].trainNum == trainNum) {
+					trainList[i].time();
+					break;
+				}
+			} else {
+				System.out.println("존재하지 않는 기차 번호 입니다. 메뉴로 돌아갑니다.");
 				break;
 			}
 		}
 	}
 
 	public void list() {
-		
+		for(int i = 0; i < tL; i++) {
+			if(i%2==0) {
+				trainList[i].prt();
+			} else {
+				trainList[i].trainType += "*";
+				trainList[i].prt();
+			}
+		}
 	}
 	
 	public void edit() {
-		
+		int[] trainNumArray = {1111, 2222, 3333, 4444};
+
+		System.out.println("기차 시간 수정이 필요한 기차 번호를 입력하시오.");
+		int editNumber = in.nextInt();
+		in.nextLine();	
+		if(IntStream.of(trainNumArray).anyMatch(x -> x == editNumber)) {
+			System.out.println("기차 시간 수정이 필요한 도착 시간을 입력하시오.");
+			String editTime = in.nextLine();
+			for(int i = 0; i < tL; i++) {
+				if(trainList[i].trainNum == editNumber) {
+					System.out.println(i);
+					if(trainList[i].trainTime.equals(editTime)) {
+						System.out.println("수정된 시간을 입력하시오.");
+						trainList[i].time();
+						break;
+					} else {
+						System.out.println("입력하신 정보가 일치하지 않습니다.");
+					}
+				}
+			}
+		} else {
+			System.out.println("존재하지 않는 기차 번호 입니다. 메뉴로 돌아갑니다.");
+		}
 	}
 }
